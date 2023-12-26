75262ae1b7e81258f8d70c0965b4dc577d9cda02
marco-cheung <marco1992@gmail.com>
2023-12-26 12:16:54 +0800

    update main file path to app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# Import libraries
+import streamlit as st
+import pandas as pd
+
+# Page setup
+st.set_page_config(page_title="Running Photos - Bib Number Search Engine", page_icon="🏃", layout="wide")
+st.title("Bib Number Search Engine")
+
+# Connect to the Google Sheet
+sheet_id = "1AvZtnDy43gr6ttpokX-w5F5s-4KpapjFgQaR6tKkxgk"
+sheet_name = "hzmbhm2023"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&sheet={sheet_name}"
+df = pd.read_csv(url, dtype=str)
+
+# Show the dataframe (we'll delete this later)
+st.write(df)
