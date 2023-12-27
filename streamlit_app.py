5b3d958264d4ceb01661e25101b74810f8c4e99a
Marco Cheung <marco1992@gmail.com>
2023-12-27 14:14:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,5 @@ else:
             st.write("---")
             cols = st.columns(N_cards_per_row, gap="large")
         with cols[n_row%N_cards_per_row]:
-            st.caption(f"{row['event'].strip()} - {row['event_time'].strip()} ")
+            st.caption(f"{row['event_time'].strip()} - {row['event_time'].strip()} ")
             st.image(row['image_path'], width=180)
