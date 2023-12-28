87d174602c2075b285aad31f2f208fe769132327
marco-cheung <marco1992@gmail.com>
2023-12-28 23:04:33 +0800

    Update page navigation column widths

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if len(df) % images_per_page:
 
 
 # Add buttons for page navigation
-col1, col2, col3, col4, col5 = st.columns([8,8,.6,.5,.3])
+col1, col2, col3, col4, col5 = st.columns([8,8,.6,.7,.3])
 
 # Define functions to increment and decrement page number
 def increment_page():
