b02c423c301a4bdf73bda11f6f85dea3e2242280
marco-cheung <marco1992@gmail.com>
2023-12-29 23:09:07 +0800

    Refactor pagination logic in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,16 +109,14 @@ if text_search:
             st.markdown(f"<p style='font-size:18px;'>No photos were found, try searching with part of the number.<br>唔好意思搵唔到相，試下輸入部分號碼搜尋。</p>", unsafe_allow_html=True)
 
     with col4:
-    # Display the current page number out of the total number of pages
-    # 'f' before the string indicates that it's a formatted string literal
         current_page = st.session_state.page
         st.markdown(f"<p style='font-size:18px;'>{current_page}/{total_pages_search}</p>", unsafe_allow_html=True)
 
 # If no input of text_search...
 else:
     with col4:
-    # Display the current page number out of the total number of pages
-    # 'f' before the string indicates that it's a formatted string literal
+        #Set the current page to 1
+        st.session_state.page = 1
         current_page = st.session_state.page
         st.markdown(f"<p style='font-size:18px;'>{current_page}/{total_pages}</p>", unsafe_allow_html=True)
 
