02419cc02df96a5a0821504ca1865d83b94db710
Marco Cheung <marco1992@gmail.com>
2023-12-29 12:15:55 +0800

    Update image width in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Running Photos - Bib Number Search 號碼布搵�
 # Create three columns with different widths on the same row
 col_a, col_b, col_c = st.columns([.3,1,2])
 
-col_a.image("https://raw.githubusercontent.com/marco-cheung/runner-tag-streamlit/main/.streamlit/running-bib-icon.png", width=100)
+col_a.image("https://raw.githubusercontent.com/marco-cheung/runner-tag-streamlit/main/.streamlit/running-bib-icon.png", width=50)
 with col_b:
    st.title("Bib Number Search 號碼布搵相")
 
