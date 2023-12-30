f9c5a6db9fe3c857d696725c4dd62660166bc16b
marco-cheung <marco1992@gmail.com>
2023-12-30 21:22:49 +0800

    Update footer style and remove extra newline

--- streamlit_app.py
+++ streamlit_app.py
@@ -199,7 +199,7 @@ display_page_navigation(col_h, col_i, col_j, col_h_key, col_j_key)
 
 # Add footer
 st.markdown("""
-    <hr style='border: dashed 1px lightgrey;'>
-    🧩Created by: Marco Cheung <br>
+    <hr style='border: dashed 1px lightgrey; margin-bottom: 0;'>
+    🧩Created by: Marco Cheung 
     📷Photo Source: Official Organizers
 """, unsafe_allow_html=True)
