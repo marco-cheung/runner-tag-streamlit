19c88340b5f254366b71384166253317cc4f5ca7
marco-cheung <marco1992@gmail.com>
2023-12-30 16:59:05 +0800

    Fix usecols parameter in load_df function

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&shee
 
 @st.cache_data
 def load_df(sheet_url):
-    data = pd.read_csv(sheet_url, dtype=str)
+    data = pd.read_csv(sheet_url, dtype=str, usecols=[:-1])
 
 df = pd.read_csv(url, dtype=str)
 
