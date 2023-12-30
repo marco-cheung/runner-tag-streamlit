fd518db76d2ede0eadc8166394bfa611a34b0d94
marco-cheung <marco1992@gmail.com>
2023-12-30 18:36:32 +0800

    Update scroll top value in scrollToTop function

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,7 +106,7 @@ js = f"""
     function scrollToTop(uniqueValue){{
         var textAreas = parent.document.querySelectorAll('section.main');
         for (let index = 0; index < textAreas.length; index++) {{
-            textAreas[index].scrollTop = 100;
+            textAreas[index].scrollTop = 50;
         }}
     }}
 </script>
