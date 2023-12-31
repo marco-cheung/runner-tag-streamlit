e8904782d86f0bebc37cb14104b47ab20b2c7f3c
marco-cheung <marco1992@gmail.com>
2023-12-31 12:54:34 +0800

    Refactor CSS styling for column layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,16 +117,6 @@ st.write('''<style>
     flex: 1 1 calc(33.3333% - 1rem) !important;
     min-width: calc(33% - 1rem) !important;
 }
-[data-testid="column"]:nth-child(2) {
-    width: calc(33.3333% - 1rem) !important;
-    flex: 1 1 calc(33.3333% - 1rem) !important;
-    min-width: calc(33% - 1rem) !important;
-}
-[data-testid="column"]:nth-child(3) {
-    width: calc(33.3333% - 1rem) !important;
-    flex: 1 1 calc(33.3333% - 1rem) !important;
-    min-width: calc(33% - 1rem) !important;
-}
 </style>''', unsafe_allow_html=True)
 
 # Add buttons for page navigation
