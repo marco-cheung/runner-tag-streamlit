0bf9ecd5f0af8626e2335b39f6f5411baf540dfe
marco-cheung <marco1992@gmail.com>
2023-12-31 14:45:53 +0800

    Update column width in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,9 +123,9 @@ st.write('''<style>
 st.write('''<style>
 [data-testid="column"][class="st-emotion-cache-hwawiw e1f1d6gn3"]
 {
-    width: calc(40% - 1rem) !important;
-    flex: 1 1 calc(40% - 1rem) !important;
-    min-width: calc(40% - 1rem) !important;
+    width: calc(30% - 1rem) !important;
+    flex: 1 1 calc(30% - 1rem) !important;
+    min-width: calc(30% - 1rem) !important;
 }
 </style>''', unsafe_allow_html=True)
 
